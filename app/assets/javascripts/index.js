54b9b758fc358587e2f7e94370988f28a60ed7e8
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-24 17:20:36 +0100

    improves esthetic

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const pad = document.querySelector(`.pad[data-key="${e.keyCode}"]`);
+  pad.classList.remove('playing'); // prevent stuck class
   if(!audio) return;
   audio.currentTime = 0;
   audio.play();
