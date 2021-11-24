2e165744a4dcd55c727cdeffe27da62a1aa3e3ba
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-24 17:10:16 +0100

    adds autoremove class playing

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const pad = document.querySelector(`.pad[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  pad.classList.add('playing');
+});
+
+function removeEffect(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+};
+
+const pads = document.querySelectorAll('.pad');
+pads.forEach(pad => pad.addEventListener('transitionend', removeEffect));
